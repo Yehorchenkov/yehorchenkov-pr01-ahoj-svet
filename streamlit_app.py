b4b8208a28fcf9dde0d00337be34a3d34a048718
Oleksii Yehorchenkov <alexee@ukr.net>
2024-10-23 08:37:03 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,67 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
+st.title("🎈 Ahoj Svet a Streamlit")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Toto je moja prvá aplikácia v Streamlite"
 )
+
+data = pd.DataFrame({
+    "Kategorie" : ["A", "B", "C", "D"],
+    "Hodnoty" : [25, 40, 35, 30]
+})
+
+st.write("Tabulka s dátami:")
+st.dataframe(data)
+
+fig = px.bar(
+    data, 
+    x="Kategorie", 
+    y="Hodnoty",
+    title="Stlpcovy graf plotly"
+)
+
+st.plotly_chart(fig)
+
+if st.button("Informacne okno"):
+    st.info("Toto je informacne okno")
+
+if st.button("Upozornovacie okno"):
+    st.warning("Toto je upozornovacie okno")
+
+if st.button("Chybove okno"):
+    st.error("Toto je chybove okno")
+
+user_input = st.text_input("Ako sa volas: ")
+st.write(f"Volas sa {user_input}")
+
+edited_data = st.data_editor(
+    data, 
+    num_rows="dynamic",
+    use_container_width=True
+)
+
+st.write("Upravena nova tabulka: ")
+st.dataframe(edited_data)
+
+st.write("Stlpcovy graf: ")
+
+fig_bar = px.bar(
+    edited_data, 
+    x="Kategorie", 
+    y="Hodnoty",
+    title="Upraveny stlpcovy graf plotly"
+)
+st.plotly_chart(fig_bar)
+
+st.write("👌 Kolacovy graf: ")
+
+fig_pie = px.pie(
+    edited_data, 
+    names="Kategorie", 
+    values="Hodnoty",
+    title="Upraveny kolacovy graf plotly"
+)
+st.plotly_chart(fig_pie)
